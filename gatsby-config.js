ce0410b49c6e18a2469fb01d41804d6c1a0674f1
Edu Depetris <edu.depetris.00@gmail.com>
2019-08-17 17:49:52 -0300

    Update favicon and remove no longer used images

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,18 +18,21 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `El Dólar`,
+        short_name: `El dólar`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/eldolar-icon.png`,
       },
     },
-    `gatsby-plugin-sass`
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }
