c16a6b4541bd713038ea4a0c17aafcab202bdc12
Edu Depetris <edu.depetris.00@gmail.com>
2019-08-17 17:54:46 -0300

    Update page structure :wrench:

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    © {new Date().getFullYear()}, Built with ❤️
+  </footer>
+)
+
+Footer.propTypes = {
+}
+
+Footer.defaultProps = {
+}
+
+export default Footer
